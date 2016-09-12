6e879f0be9a1cf30e58cabb4fc6b19942af34514
David Schoenbauer <dschoenbauer@coetruman.com>
2016-09-12 09:25:27 -0500

    #9 close #7

--- src/Listener/Execute.php
+++ src/Listener/Execute.php
@@ -1,116 +1,11 @@
 <?php
 namespace Dschoenbauer\SqlControl\Listener;
 
-use Dschoenbauer\SqlControl\Components\SqlGroup;
 use Dschoenbauer\SqlControl\Components\SqlChange;
-use Dschoenbauer\SqlControl\Enum\Attributes;
-use Dschoenbauer\SqlControl\Enum\Events;
 use Dschoenbauer\SqlControl\Components\SqlControlManager;
-use Dschoenbauer\SqlControl\Status\Fail;
-use Dschoenbauer\SqlControl\Status\Skipped;
-use Dschoenbauer\SqlControl\Status\Success;
-use Dschoenbauer\SqlControl\Visitor\VisitorInterface;
-use Exception;
-use PDO;
-use Zend\EventManager\Event;
-
-/**
- * Description of Execute
- *
- * @author David Schoenbauer <dschoenbauer@gmail.com>
- */
-class Execute implements VisitorInterface
-{
-
-    private $_sqlControlManager;
-
-    public function visitSqlControlManager(SqlControlManager $sqlControlManager)
-    {
-        $sqlControlManager->getEventManager()->attach(Events::EXECUTE, [$this, 'onExecute']);
-    }
-
-    public function onExecute(Event $event)
-    {
-        $this->setSqlControlManager($event->getTarget());
-        $groups = $this->getSqlControlManager()->getAttributes()->getValue(Attributes::GROUPS, []);
-        foreach ($groups as $group) {
-            $this->executeGroup($group, $this->getSqlControlManager()->getAdapter());
-        }
-    }
-
-    private function executeGroup(SqlGroup $group, PDO $adapter)
-    {
-        $versions = $group->getSqlChanges();
-        $noErrors = true;
-        /* @var $version SqlChange */
-        foreach ($versions as $version) {
-            try {
-                if(!$version->getStatus()->isPendingLoad()){
-                    continue;
-                }
-                if ($noErrors) {
-                    $adapter->beginTransaction();
-                    $this->executeSql($adapter, $version->getStatements());
-                    $version->setStatus(new Success());
-                    $adapter->commit();
-                }else{
-                    $version->setStatus(new Skipped());
-                }
-            } catch (Exception $exc) {
-                $adapter->rollBack();
-                $noErrors = false;
-                $version->setStatus(new Fail());
-                $version->getAttributes()->add('error', ' - ' . $exc->getMessage());
-            }
-            $this->logStatus($version);
-        }
-    }
-
-    private function logStatus(SqlChange $sqlChange)
-    {
-        $message = "{status} - {file}{error}";
-        $context = [
-            'file' => $sqlChange->getName(),
-            'status' => $sqlChange->getStatus()->getName(),
-            'success' => $sqlChange->getStatus()->isLoaded(),
-            'error' => $sqlChange->getAttributes()->getValue('error'),
-        ];
-        $em = $this
-            ->getSqlControlManager()
-            ->getEventManager();
-
-        $em->trigger(Events::LOG_INFO, $this->getSqlControlManager(), compact('message', 'context'));
-        $em->trigger(Events::RESULT, $this->getSqlControlManager(), $context);
-    }
-
-    private function executeSql(\PDO $adapter, $sqls)
-    {
-        foreach ($sqls as $sql) {
-            $adapter->prepare($sql)->execute();
-        }
-    }
-
-    /**
-     * @return SqlControlManager
-     */
-    public function getSqlControlManager()
-    {
-        return $this->_sqlControlManager;
-    }
-
-    public function setSqlControlManager($sqlControlManager)
-    {
-        $this->_sqlControlManager = $sqlControlManager;
-        return $this;
-    }
-}
-namespace Dschoenbauer\SqlControl\Listener;
-
 use Dschoenbauer\SqlControl\Components\SqlGroup;
-use Dschoenbauer\SqlControl\Components\SqlChange;
 use Dschoenbauer\SqlControl\Enum\Attributes;
 use Dschoenbauer\SqlControl\Enum\Events;
-use Dschoenbauer\SqlControl\SqlControlManager;
 use Dschoenbauer\SqlControl\Status\Fail;
 use Dschoenbauer\SqlControl\Status\Skipped;
 use Dschoenbauer\SqlControl\Status\Success;
